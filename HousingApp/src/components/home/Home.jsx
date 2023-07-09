3fd8272dd573ea225eca0a2e0121a51d2f04baed
cotsiosglad <c.constantinides95@gmail.com>
2023-07-09 20:14:28 +0300

    Added banner1 picture, removed some bs

--- HousingApp/src/components/home/Home.jsx
+++ HousingApp/src/components/home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react"
-import Awards from "./awards/Awards"
 import Featured from "./featured/Featured"
 import Hero from "./hero/Hero"
 import Location from "./location/Location"
-import Price from "./price/Price"
 import Recent from "./recent/Recent"
-import Team from "./team/Team"
+// import Price from "./price/Price"
+// import Awards from "./awards/Awards"
+// import Team from "./team/Team"
 
 const Home = () => {
   return (
@@ -13,10 +13,10 @@ const Home = () => {
       <Hero />
       <Featured />
       <Recent />
-      <Awards />
+      {/* <Awards /> */}
       <Location />
-      <Team />
-      <Price />
+      {/* <Team />
+      <Price /> */}
     </>
   )
 }
