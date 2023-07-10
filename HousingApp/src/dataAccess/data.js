ac2285ae72c08c68467d9c7bcf72209598abe827
cotsiosglad <c.constantinides95@gmail.com>
2023-07-11 00:46:50 +0300

    Deployed to firebase, need to build with webpack to redirect hosting

--- HousingApp/src/dataAccess/data.js
+++ HousingApp/src/dataAccess/data.js
@@ -1,5 +1,3 @@
-import "./App.css"
-import Pages from "./components/pages/Pages"
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -21,9 +19,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-function App() {
-  return <Pages />
-}
-
-export default App
-
+export default app;
