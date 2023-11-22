37c898ab3d5c737d18f190a31161532e8ac4df43
Nikolas09 <nikolas_erotokritou@hotmail.co.uk>
2023-11-22 23:08:31 +0200

    meta tags added to seo component

--- HousingApp/src/customHelperComponents/SeoComponent.js
+++ HousingApp/src/customHelperComponents/SeoComponent.js
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet';
 import React from 'react';
+
 const Seo = ({ title, description, pathSlug, keywords }) => {
   const url = `https://domusalba.eu/${pathSlug}`
 	return (
@@ -9,10 +10,33 @@ const Seo = ({ title, description, pathSlug, keywords }) => {
           content: description,
         },
         {
-            name: 'og_image',
-            property: 'og:image',
-            content: "https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw",
-          },
+          property: 'og:title',
+          content: description,
+        },
+        {
+          name:"og_url",
+          property: 'og:url',
+          content: url,
+        },
+        {
+          name:"og_type",
+          property: 'og:type',
+          content: "project",
+        },
+        {
+          name: 'og_image',
+          property: 'og:image',
+          content: "https://firebasestorage.googleapis.com/v0/b/housing-app-628b7.appspot.com/o/images%2FDA_final%20logo-black.png?alt=media&token=81dcc63e-3448-4bd4-876b-6f792e338871",
+        },
+        {
+          name:"og_image_width",
+          property: 'og:image:width',
+          content: "1000",
+        },
+        {
+          property: 'og:image',
+          content: "https://firebasestorage.googleapis.com/v0/b/housing-app-628b7.appspot.com/o/images%2FDA_final%20logo-black.png?alt=media&token=81dcc63e-3448-4bd4-876b-6f792e338871",
+        },
         // {
         //   name: 'keywords',
         //   content: keywords.join(),
